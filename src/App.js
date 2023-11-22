ced03ff3d3494c0136d6774ad42885963b9f4111
Jomi Susan Mathew <73014817+jomi123@users.noreply.github.com>
2023-11-22 13:05:03 +0530

    Done: basic functions

--- src/App.js
+++ src/App.js
@@ -1,23 +1,141 @@
-import logo from './logo.svg';
-import './App.css';
+import { useState } from "react";
+import "./App.css";
 
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
+  const [jsbutton, setjsbutton] = useState();
+  const [fictionbutton, setfictionbutton] = useState();
+  const [business, setbusiness] = useState();
+
+  function handleJavascriptOnClick() {
+    setjsbutton(
+      <div>
+        <div
+          style={{
+            padding: "1rem",
+            border: "1px solid black",
+            borderRadius: "0.5rem",
+            width: "20rem",
+          }}
         >
-          Learn React
-        </a>
-      </header>
+          <div>Eloquent JavaScript</div>
+          4/5
+        </div>{" "}
+        <br></br>
+        <div
+          style={{
+            padding: "1rem",
+            border: "1px solid black",
+            borderRadius: "0.5rem",
+            width: "20rem",
+          }}
+        >
+          <div>You Don't Know JS</div>
+          3.5/5
+        </div>
+      </div>
+    );
+  }
+
+  function handleFictionOnClick() {
+    setfictionbutton(
+      <div>
+        <div
+          style={{
+            padding: "1rem",
+            border: "1px solid black",
+            borderRadius: "0.5rem",
+            width: "20rem",
+          }}
+        >
+          <div>Shiva Trilogy</div>
+          5/5
+        </div>{" "}
+        <br></br>
+        <div
+          style={{
+            padding: "1rem",
+            border: "1px solid black",
+            borderRadius: "0.5rem",
+            width: "20rem",
+          }}
+        >
+          <div>Harry Potter and the Sorcerer's Stone</div>
+          4.5/5
+        </div>
+      </div>
+    );
+  }
+
+  function handleBusinessOnClick() {
+    setbusiness(
+      <div>
+        <div
+          style={{
+            padding: "1rem",
+            border: "1px solid black",
+            borderRadius: "0.5rem",
+            width: "20rem",
+          }}
+        >
+          <div>Never Split the Difference</div>
+          3.5/5
+        </div>{" "}
+        <br></br>
+        <div
+          style={{
+            padding: "1rem",
+            border: "1px solid black",
+            borderRadius: "0.5rem",
+            width: "20rem",
+          }}
+        >
+          <div>Loonshots</div>
+          5/5
+        </div>
+      </div>
+    );
+  }
+  return (
+    <div style={{ textAlign: "left", padding: "2rem" }}>
+      <h3 style={{ fontSize: "2rem" }}>📚goodbooks</h3>
+      <h5>Checkout my favorite books. Select a genre to get started</h5>
+      <button
+        style={{
+          padding: "0.5rem",
+          borderRadius: "0.5rem",
+          margin: "0.4rem",
+          cursor: "pointer",
+        }}
+        onClick={handleJavascriptOnClick}
+      >
+        Javascript
+      </button>
+      <button
+        style={{
+          padding: "0.5rem",
+          borderRadius: "0.5rem",
+          margin: "0.4rem",
+          cursor: "pointer",
+        }}
+        onClick={handleFictionOnClick}
+      >
+        Fiction
+      </button>
+      <button
+        style={{
+          padding: "0.5rem",
+          borderRadius: "0.5rem",
+          margin: "0.4rem",
+          cursor: "pointer",
+        }}
+        onClick={handleBusinessOnClick}
+      >
+        Business
+      </button>
+      <hr></hr>
+      {jsbutton}
+      {fictionbutton}
+      {business}
     </div>
   );
 }
